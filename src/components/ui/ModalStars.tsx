f619a6cb961cd97c5dac83f8c25a1e3b33887776
Eultype <Sdarryy59@gmail.com>
2026-01-27 20:30:55 +0100

    fix(project): ajustement du background de la modale

--- src/components/ui/ModalStars.tsx
+++ src/components/ui/ModalStars.tsx
@@ -4,9 +4,10 @@ import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
+import * as THREE from 'three';
 
 function Stars(props: any) {
-    const ref = useRef<any>();
+    const ref = useRef<THREE.Points>(null);
     
     // 6000 valeurs / 3 = 2000 étoiles exactement
     // Cela évite l'erreur NaN car le tableau est complet
